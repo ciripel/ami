c2ddba38b84d7827e633b67b31a8af696146e02a
cryi <cryi@tutanota.com>
2020-02-19 17:16:45 +0100

    updated install script

--- install.sh
+++ install.sh
@@ -6,7 +6,9 @@ wget https://raw.githubusercontent.com/cryon-io/eli/master/install.sh -O /tmp/in
 # install ami
 LATEST=$(curl -sL https://api.github.com/repos/cryon-io/ami/releases/latest | grep tag_name | sed 's/  "tag_name": "//g' | sed 's/",//g')
 
-wget "https://github.com/cryon-io/ami/releases/download/$LATEST/ami.lua" -O eli &&
-    mv eli /usr/sbin/ami &&
+TMP_NAME="/tmp/$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)"
+
+wget "https://github.com/cryon-io/ami/releases/download/$LATEST/ami.lua" -O "$TMP_NAME" &&
+    mv "$TMP_NAME" /usr/sbin/ami &&
     chmod +x /usr/sbin/ami &&
     echo "ami $LATEST successfuly installed."
