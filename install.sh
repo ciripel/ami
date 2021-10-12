9e964423ce3a8afdbd3db319c3e1ef0865a80091
cryi <cryi@tutanota.com>
2021-10-12 20:44:12 +0200

    fixed hash comparison

--- install.sh
+++ install.sh
@@ -4,10 +4,10 @@ TMP_NAME="/tmp/$(head -n 1 -c 32 /dev/urandom | tr -dc 'a-zA-Z0-9'| fold -w 32)"
 
 if which curl >/dev/null; then
     set -- curl -L --progress-bar -o "$TMP_NAME"
-    LATEST=$(curl -sL https://api.github.com/repos/cryon-io/ami/releases/latest | grep tag_name | sed 's/  "tag_name": "//g' | sed 's/",//g')
+    LATEST=$(curl -sL https://api.github.com/repos/alis-is/ami/releases/latest | grep tag_name | sed 's/  "tag_name": "//g' | sed 's/",//g')
 else
     set -- wget -q --show-progress -O "$TMP_NAME"
-    LATEST=$(wget -qO- https://api.github.com/repos/cryon-io/ami/releases/latest | grep tag_name | sed 's/  "tag_name": "//g' | sed 's/",//g')
+    LATEST=$(wget -qO- https://api.github.com/repos/alis-is/ami/releases/latest | grep tag_name | sed 's/  "tag_name": "//g' | sed 's/",//g')
 fi
 
 # install eli
@@ -23,7 +23,7 @@ fi
 
 # install ami
 echo "Downloading ami $LATEST..."
-if "$@" "https://github.com/cryon-io/ami/releases/download/$LATEST/ami.lua" &&
+if "$@" "https://github.com/alis-is/ami/releases/download/$LATEST/ami.lua" &&
     mv "$TMP_NAME" /usr/sbin/ami &&
     chmod +x /usr/sbin/ami; then
     echo "ami $LATEST successfuly installed."
