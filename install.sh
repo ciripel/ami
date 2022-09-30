a24a368cfa952355d03b901f4bf1f8751bfed437
cryi <cryi@tutanota.com>
2022-09-30 13:42:38 +0200

    support install on platforms without progress

--- install.sh
+++ install.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
-TMP_NAME="/tmp/$(head -n 1 -c 32 /dev/urandom | tr -dc 'a-zA-Z0-9'| fold -w 32)"
+TMP_NAME="./$(head -n 1 -c 32 /dev/urandom | tr -dc 'a-zA-Z0-9'| fold -w 32)"
 
 if which curl >/dev/null; then
-    set -- curl -L --progress-bar -o "$TMP_NAME"
+    if curl --help | grep "--progress-bar"; then 
+        PROGRESS="--progress-bar"
+    fi
+
+    set -- curl -L $PROGRESS -o "$TMP_NAME"
     LATEST=$(curl -sL https://api.github.com/repos/alis-is/ami/releases/latest | grep tag_name | sed 's/  "tag_name": "//g' | sed 's/",//g')
 else
-    set -- wget -q --show-progress -O "$TMP_NAME"
+    if wget --help | grep "--show-progress"; then 
+        PROGRESS="--show-progress"
+    fi
+    set -- wget -q $PROGRESS -O "$TMP_NAME"
     LATEST=$(wget -qO- https://api.github.com/repos/alis-is/ami/releases/latest | grep tag_name | sed 's/  "tag_name": "//g' | sed 's/",//g')
 fi
 
@@ -29,10 +36,12 @@ fi
 # install ami
 echo "Downloading ami $LATEST..."
 if "$@" "https://github.com/alis-is/ami/releases/download/$LATEST/ami.lua" &&
-    mv "$TMP_NAME" /usr/sbin/ami &&
+    cp "$TMP_NAME" /usr/sbin/eli &&
     chmod +x /usr/sbin/ami; then
+    rm "$TMP_NAME"
     echo "ami $LATEST successfuly installed."
 else
+    rm "$TMP_NAME"
     echo "ami installation failed!" 1>&2
     exit 1
 fi
