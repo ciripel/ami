af88a03c65e3032bde825a0232896285e88c44af
cryi <cryi@tutanota.com>
2024-03-16 18:36:55 +0100

    support --prerelease

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 TMP_NAME="./$(head -n 1 -c 32 /dev/urandom | tr -dc 'a-zA-Z0-9'| fold -w 32)"
+PRERELEASE=false
+PRERELEASE_FLAG=""
+if [ "$1" = "--prerelease" ]; then
+    PRERELEASE=true
+    PRERELEASE_FLAG="--prerelease"
+fi
 
 if which curl >/dev/null; then
     if curl --help  2>&1 | grep "--progress-bar" >/dev/null 2>&1; then 
@@ -8,13 +14,21 @@ if which curl >/dev/null; then
     fi
 
     set -- curl -L $PROGRESS -o "$TMP_NAME"
-    LATEST=$(curl -sL https://api.github.com/repos/alis-is/ami/releases/latest | grep tag_name | sed 's/  "tag_name": "//g' | sed 's/",//g')
+    if [ "$PRERELEASE" = true ]; then
+        LATEST=$(curl -sL https://api.github.com/repos/alis-is/ami/releases | grep tag_name | sed 's/  "tag_name": "//g' | sed 's/",//g' | head -n 1 | tr -d '[:space:]')
+    else
+        LATEST=$(curl -sL https://api.github.com/repos/alis-is/ami/releases/latest | grep tag_name | sed 's/  "tag_name": "//g' | sed 's/",//g' | tr -d '[:space:]')
+    fi
 else
     if wget --help  2>&1 | grep "--show-progress" >/dev/null 2>&1; then 
         PROGRESS="--show-progress"
     fi
     set -- wget -q $PROGRESS -O "$TMP_NAME"
-    LATEST=$(wget -qO- https://api.github.com/repos/alis-is/ami/releases/latest | grep tag_name | sed 's/  "tag_name": "//g' | sed 's/",//g')
+    if [ "$PRERELEASE" = true ]; then
+        LATEST=$(wget -qO- https://api.github.com/repos/alis-is/ami/releases | grep tag_name | sed 's/  "tag_name": "//g' | sed 's/",//g' | head -n 1 | tr -d '[:space:]')
+    else
+        LATEST=$(wget -qO- https://api.github.com/repos/alis-is/ami/releases/latest | grep tag_name | sed 's/  "tag_name": "//g' | sed 's/",//g' | tr -d '[:space:]')
+    fi
 fi
 
 # install eli
@@ -24,7 +38,8 @@ if ! "$@" https://raw.githubusercontent.com/alis-is/eli/master/install.sh; then
     rm "$TMP_NAME"
     exit 1
 fi
-if ! sh "$TMP_NAME"; then
+
+if ! sh "$TMP_NAME" $PRERELEASE_FLAG; then
     echo "Failed to download eli, please retry ... "
     rm "$TMP_NAME"
     exit 1
